705afbe21fce5e66e30dfbe9e463597f13e7a042
r4v3n <salah.hamsate@gmail.com>
2024-02-17 01:24:03 +0100

    done

---
+++ Day03/ex00/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shamsate < shamsate@student.42.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/17 00:00:39 by shamsate          #+#    #+#             */
+/*   Updated: 2024/02/17 01:04:37 by shamsate         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "include/ClapTrap.hpp"
+
+int     main(void)
+{
+    ClapTrap    stive("stive");
+    ClapTrap    amanda("amanda");
+
+    stive.setDamage(4);
+    stive.attack(amanda.getName());
+    stive.attack(amanda.getName());
+    amanda.takeDamage(1);
+    amanda.beRepaired(100);
+    amanda.attack(stive.getName());
+    stive.takeDamage(1);
+    stive.getStatus();
+    amanda.getStatus();
+    return (0);
+}
